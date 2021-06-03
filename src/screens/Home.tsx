3a7209fd37868cae3c7c182b81e26f8ae96d7a91
tiagobarros01 <tbarros1337@hotmail.com>
2021-06-03 11:21:00 -0300

    chore: Creates a project structure & creates a new home screen

---
+++ src/screens/Home.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text, View } from 'react-native';
+
+export default function Home() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Home screen</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
