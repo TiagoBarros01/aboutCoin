79fd11ca77db166c0c839cb04ca06d109d175166
tiagobarros01 <tbarros1337@hotmail.com>
2021-06-12 00:13:15 -0300

    fix: change endpoint of api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://api.coindesk.com/v1/bpi/historical/close.json?',
+  baseURL: 'https://api.coindesk.com/v1/bpi/historical',
 });
